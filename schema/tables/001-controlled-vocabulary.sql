15b51cee4890bd4ab036906dac2a441c4bdb356f
Justin Merz <jrmerz@gmail.com>
2025-03-24 15:36:08 -0700

    adding missing controlled vocab types

--- schema/tables/001-controlled-vocabulary.sql
+++ schema/tables/001-controlled-vocabulary.sql
@@ -30,6 +30,9 @@ INSERT INTO controlled_vocabulary_type (name) VALUES ('salinity_tolerance') ON C
 INSERT INTO controlled_vocabulary_type (name) VALUES ('shade_tolerance') ON CONFLICT DO NOTHING;
 INSERT INTO controlled_vocabulary_type (name) VALUES ('soil_tolerance') ON CONFLICT DO NOTHING;
 INSERT INTO controlled_vocabulary_type (name) VALUES ('leaf_retention') ON CONFLICT DO NOTHING;
+INSERT INTO controlled_vocabulary_type (name) VALUES ('toxicity') ON CONFLICT DO NOTHING;
+INSERT INTO controlled_vocabulary_type (name) VALUES ('species_common_name') ON CONFLICT DO NOTHING;
+INSERT INTO controlled_vocabulary_type (name) VALUES ('usda_zone') ON CONFLICT DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS controlled_vocabulary (
   controlled_vocabulary_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
